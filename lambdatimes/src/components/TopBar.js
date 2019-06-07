2e36d4dd134054d1bb28ec502437572f3ba13dfa
oloruntobi <awoderuoloruntobi@gmail.com>
2019-06-07 10:33:44 +0100

    Refactored my topbar styled components

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -6,23 +6,23 @@ import styled from "styled-components";
 
 const TopBar = () => {
   return (
-    <Top className="top-bar">
-      <div className="container">
-        <div className="container-left">
+    <Top>
+      <Container>
+        <ContainerLeft>
           <span>TOPICS</span>
           <span>SEARCH</span>
-        </div>
-        <div className="container-center">
+        </ContainerLeft>
+        <ContainerCenter>
           <span>GENERAL</span>
           <span>BROWNBAG</span>
           <span>RANDOM</span>
           <span>MUSIC</span>
           <span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
+        </ContainerCenter>
+        <ContainerRight>
           <span>LOG IN</span>
-        </div>
-      </div>
+        </ContainerRight>
+      </Container>
     </Top>
   );
 };
@@ -35,65 +35,65 @@ const Top = styled.div`
   position: fixed;
   height: 44px;
   background-color: #333;
+`
 
-  .container {
-    width: 100%;
-    display: flex;
-    justify-content: none;
-    align-items: none;
-    flex-direction: row;
-    color: #fff;
-    letter-spacing: 1px;
-    padding: 0 10px;
-    @media (min-width: 1280px) {
-
-      width: 1280px;
-    
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+  @media (min-width: 1280px) {
+    width: 1280px;
   }
-    .container-right {
-      display: flex;
-      justify-content: flex-end;
-      align-items: center;
-      flex-direction: row;
-      flex: 1;
-      font-size: 11px;
-      font-weight: bold;
-      span {
-        cursor: pointer;
-      }
-    }
-    .container-center {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: row;
-      flex: 3;
-      font-size: 9px;
-      span {
-        cursor: pointer;
-        margin-right: 5%;
-        &:hover {
-          text-decoration: underline;
-        }
-        &:last-child {
-          margin-right: 0;
-        }
-      }
-    }
+`
 
-    .container-left {
-      display: flex;
-      justify-content: none;
-      align-items: center;
-      flex-direction: row;
-      flex: 1;
-      font-size: 11px;
-      span {
-        cursor: pointer;
-        margin-right: 25%;
-        font-weight: bold;
-      }
+const ContainerLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  span {
+    cursor: pointer;
+    margin-right: 25%;
+    font-weight: bold;
+  }
+`
+
+const ContainerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+  span {
+    cursor: pointer;
+    margin-right: 5%;
+    &:last-child {
+      margin-right: 0;
+    }
+    &:hover {
+      text-decoration: underline;
     }
   }
-`;
+`
+
+const ContainerRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+  span {
+    cursor: pointer;
+  }
+`
 export default TopBar;
